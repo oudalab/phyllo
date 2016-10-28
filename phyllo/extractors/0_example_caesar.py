352b129bf62fe41dba46d1cf4c111c43deb0808c
Jordan Nguyen <jordan.nguyen-1@ou.edu>
2016-10-28 13:19:51 -0500

    Templates and Examples
    Template is derived from ciceroDB.py, and contains general cases on scraping certain or other pages.
    The Caesar example was created before the template was; however, it is better-documented than all of the other code.

---
+++ phyllo/extractors/0_example_caesar.py
@@ -0,0 +1,94 @@
+import sqlite3
+import urllib
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from phyllo_logger import logger
+
+# sets the basic information and whatnot
+siteURL = 'http://www.thelatinlibrary.com'
+caesarURL = 'http://www.thelatinlibrary.com/caes.html'
+caesarOPEN = urllib.request.urlopen(caesarURL)
+caesarSOUP = BeautifulSoup(caesarOPEN, 'html5lib')
+# empty list to store URLs of Caesar's works.
+textsURL = []
+
+# get links to books in the collection
+for a in caesarSOUP.find_all('a', href=True):
+    link = a['href']
+    textsURL.append("{}/{}".format(siteURL, a['href']))
+
+# remove unnecessary URLs
+while ("http://www.thelatinlibrary.com/index.html" in textsURL):
+    textsURL.remove("http://www.thelatinlibrary.com/index.html")
+    textsURL.remove("http://www.thelatinlibrary.com/classics.html")
+logger.info("\n".join(textsURL))
+
+# basic information for the collection
+author = caesarSOUP.title.string
+author = author.strip()
+collectiontitle = caesarSOUP.h1.contents[0].strip()
+date = caesarSOUP.h2.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
+
+with sqlite3.connect('pots.db') as db:
+    c = db.cursor()
+    c.execute(
+        'CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY, title TEXT, book TEXT,'
+        ' language TEXT, author TEXT, date TEXT, chapter TEXT, verse TEXT, passage TEXT,'
+        ' link TEXT, documentType TEXT)')
+    # clears out so that we can replace it later.
+    c.execute("DELETE FROM texts WHERE author = 'Caesar'")
+    title = ''
+    # this loop then goes through individual works.
+    for URL in textsURL:
+        openURL = urllib.request.urlopen(URL)
+        soup = BeautifulSoup(openURL, 'html5lib')
+        try:
+            title = soup.title.string.split(':')[-1].strip()
+        except:
+            pass
+        # stores a bunch of instances where <p> tags were found. So pretty much each paragraph.
+        ptags = soup.find_all('p')[:-1]
+        # the following two are initialized early since they sometimes need to carry over
+        # to the next <p> tag.
+        chapter = '-1'
+        sentence = '-1'
+        for p in ptags:
+            # make sure it's not a paragraph without the main text
+            try:
+                # these classes of <p> don't contain any information we need, so we skip over them.
+                if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                             'internal_navigation']:
+                    continue
+            except:
+                pass
+
+            # chapters are surrounded by <a> tags
+            potentialchapter = p.find('a')
+            if potentialchapter is not None:
+                chapter = potentialchapter.find(text=True)
+                if chapter.endswith(']'): # some books have ']' inside the tag
+                    chapter = chapter[:-1]
+                a = p.a.decompose() # removing it now makes splitting easier
+            text = p.get_text().strip()
+            if text.startswith('[]'): # gets rid of brackets outside the tag
+                text = text[2:].strip()
+            elif text.startswith('['):
+                text = text[1:].strip()
+            # Only the Bello Gallico is split in sentences. Others cannot be split by '.' due to abbreviations.
+            text = re.split('([0-1][0-3][0-9])|([0-9][0-9])|([0-9])', text)
+            # each unit is generally supposed to be what was split via regular expression
+            for unit in text:
+                if unit is None or unit == '' or unit.isspace():
+                    continue
+                unit = unit.strip()
+                if unit.isnumeric(): # applies to subsection number in Bellum Gallicum
+                    sentence = unit
+                    continue
+                else:
+                    # one occasion where title was found in the header tag and not title tag.
+                    if title == 'The Latin Library':
+                        title = soup.h1.string.strip()
+                    c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                              (None, collectiontitle, title, 'Latin', author, date, chapter,
+                               sentence, unit, URL, 'prose'))
---
+++ phyllo/extractors/0_template.py
@@ -0,0 +1,232 @@
+import sqlite3
+import urllib
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from phyllo_logger import logger
+
+
+# Case 1: Sections split by numbers (Roman or not) followed by a period, or bracketed. Subsections split by <p> tags
+def parsecase1(ptags, c, colltitle, title, author, date, URL):
+    # ptags contains all <p> tags. c is the cursor object.
+    chapter = '-1'
+    verse = 1
+
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        passage = ''
+        text = p.get_text().strip()
+        # Skip empty paragraphs. and skip the last part with the collection link.
+        if len(text) <= 0 or text.startswith('Cicero\n'):
+            continue
+        text = re.split('^([IVX]+)\.\s|^([0-9]+)\.\s|^\[([IVXL]+)\]\s|^\[([0-9]+)\]\s', text)
+        for element in text:
+            if element is None or element == '' or element.isspace():
+                text.remove(element)
+        # The split should not alter sections with no prefixed roman numeral.
+        if len(text) > 1:
+            i = 0
+            while text[i] is None:
+                i+=1
+            chapter = text[i]
+            i+=1
+            while text[i] is None:
+                i+=1
+            passage = text[i].strip()
+            verse = 1
+        else:
+            passage = text[0]
+            verse+=1
+        # check for that last line with the author name that doesn't need to be here
+        if passage.startswith('Florus'):
+            continue
+        c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                  (None, colltitle, title, 'Latin', author, date, chapter,
+                   verse, passage.strip(), URL, 'prose'))
+
+
+
+# Case 2: Sections are split by <p> tags and subsections by un/bracketed numbers.
+def parsecase2(ptags, c, colltitle, title, author, date, URL):
+    # ptags contains all <p> tags. c is the cursor object.
+    chapter = 0
+    verse = '-1'
+    # entry deletion is done in main()
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        passage = ''
+        text = p.get_text().strip()
+        if text.startswith("Cicero\n"):
+            continue
+        # Skip empty paragraphs.
+        if len(text) <= 0:
+            continue
+        text = re.split('([IVX]+)\.\s|([0-9]+)\.\s|\[([IVXL]+)\]\s|\[([0-9]+)\]\s', text)
+        for element in text:
+            if element is None or element == '' or element.isspace():
+                text.remove(element)
+        chapter +=1
+        for count, item in enumerate(text):
+            if item is None:
+                continue
+            if item.isnumeric() or len(item) < 5:
+                verse = item
+            else:
+                passage = item
+                c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                          (None, colltitle, title, 'Latin', author, date, chapter,
+                           verse, passage.strip(), URL, 'prose'))
+
+
+# Case 3: Chapters separated by un/bracketed numbers, similarly to sentences.
+def parsecase3(ptags, c, colltitle, title, author, date, URL):
+    chapter = -1
+    verse = -1
+    isnumeral = case3isNumeral(ptags)
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        passage = ''
+        text = p.get_text().strip()
+        # Skip empty paragraphs.
+        if len(text) <= 0:
+            continue
+        text = re.split('([IVXL]+)\.\s|([0-9]+)\.\s|\[([IVXL]+)\]\s|\[([0-9]+)\]\s', text)
+        for item in text:
+            if item is None:
+                continue
+            item = item.strip()
+            if item.isspace() or item == '' or item.startswith("Cicero\n"):
+                continue
+            if item.isdigit() and not isnumeral:
+                chapter = item
+            elif item.isdigit() and isnumeral:
+                verse = item
+            elif len(item) < 5 and isnumeral:
+                chapter = item
+            else:
+                passage = item
+                # Remove brackets if they have been picked up.
+                if chapter.startswith('['):
+                    chapter = chapter[:-1]
+                    chapter = chapter[1:]
+                if passage.startswith('['):
+                    passage = passage[:-1]
+                    passage = passage[1:]
+                if passage == chapter:
+                    continue
+                else: # chapter/passage correction
+                    chaptertest = chapter + 'I'
+                    chaptertest2 = chapter[:-2] + 'V'
+                    chaptertest3 = chapter[:-3] + 'V'
+                    chaptertest4 = chapter[:-4] + 'IX'
+                    chaptertest5 = chapter[:-2] + 'X'
+                    if (chapter == 'LXIX' or chapter == 'LXX') and passage == 'L':
+                        continue
+                    if passage == chaptertest or passage == chaptertest2 or passage == chaptertest3\
+                            or passage == chaptertest4 or passage == chaptertest5:
+                        chapter = passage
+                        continue
+                c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                          (None, colltitle, title, 'Latin', author, date, chapter,
+                           verse, passage.strip(), URL, 'prose'))
+
+
+# this function checks if the work uses Roman Numerals or numerical values for chapters.
+def case3isNumeral(ptags):
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        if p.get_text().strip() is None or p.get_text().strip() == '':
+            continue
+        firstp = p.get_text().strip()
+        break
+    firstp = re.split('^([IVX]+)\.\s|^([0-9]+)\.\s|^\[([IVXL]+)\]\s|^\[([0-9]+)\]\s', firstp)
+    if firstp[0] is not None:
+        if firstp[0].isdigit():
+            return False
+        else:
+            return True
+    elif firstp[1] is not None:
+        if firstp[1].isdigit():
+            return False
+        else:
+            return True
+    elif firstp[2] is not None:
+        if firstp[2].isdigit():
+            return False
+        else:
+            return True
+
+
+def getBooks(soup):
+    siteURL = 'http://www.thelatinlibrary.com'
+    textsURL = []
+    # get links to books in the collection
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        textsURL.append("{}/{}".format(siteURL, a['href']))
+
+    # remove unnecessary URLs
+    while ("http://www.thelatinlibrary.com/index.html" in textsURL):
+        textsURL.remove("http://www.thelatinlibrary.com/index.html")
+        textsURL.remove("http://www.thelatinlibrary.com/classics.html")
+    logger.info("\n".join(textsURL))
+    return textsURL
+
+
+# main code
+def main():
+    # The collection URL below. In this example, we have a link to Cicero.
+    collURL = 'http://www.thelatinlibrary.com/cic.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = collSOUP.h1.string.strip()
+    date = collSOUP.h2.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
+
+    textsURL = getBooks(collSOUP)
+
+    with sqlite3.connect('texts.db') as db:
+        c = db.cursor()
+        c.execute("DELETE FROM texts WHERE author='Cicero'")
+        for url in textsURL:
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            try:
+                title = textsoup.title.string.split(':')[1].strip()
+            except:
+                title = textsoup.title.string.strip()
+            getp = textsoup.find_all('p')
+            # finally, pick ONE case to parse with.
+
+            # parsecase1(getp, c, colltitle, title, author, date, url)
+            # parsecase2(getp, c, colltitle, title, author, date, url)
+            # parsecase3(getp, c, colltitle, title, author, date, url)
+
+    logger.info("Program runs successfully.")
+
+
+
