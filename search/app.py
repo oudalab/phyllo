8c92e5dc2a88eebc0466911e523af93f8331842d
Ram Charran Tummala <Ram Charran Tummala>
2017-02-02 11:10:21 -0600

    added the new app.py

--- search/app.py
+++ search/app.py
@@ -1,34 +1,42 @@
-
 import json
 
 import apsw
 import sqlitefts as fts
+import os
 
 import search
 from search import OUWordTokenizer
-import phyllo  # phyllo must be installed
+#import phyllo  # phyllo must be installed
+#from pympler.tracker import SummaryTracker
 
 from flask import Flask
 app = Flask(__name__)
 
-
+#tracker = SummaryTracker()
 def tokenize():
-    with apsw.Connection('texts.db') as connection:
+        connection = apsw.Connection('texts.db', flags=apsw.SQLITE_OPEN_READWRITE)
         c = connection.cursor()
+        print("connection to cursor")
         fts.register_tokenizer(c, 'oulatin', fts.make_tokenizer_module(OUWordTokenizer('latin')))
-        c.create("CREATE VIRTUAL TABLE text_idx USING fts4 (id, title, book, author, date, chapter, "
-                 "verse, passage, link, documentType, tokenize={}".format("oulatin"))
-        c.execute("INSERT INTO text_idx (id title, book, author, date, chapter, "
-                 "verse, passage, link, documentType) SELECT id, title, book, author, date, chapter, "
-                 "verse, passage, link, documentType FROM texts")
-
+        print("registering tokenizer")
+        c.execute("begin;")
+        c.execute("CREATE VIRTUAL TABLE IF NOT EXISTS text_idx  USING fts3 (id, title, book, author, date, chapter, verse, passage, link, documentType, tokenize={});".format("oulatin"))
+        c.execute("commit;")
+        print("virtual table created")
+        c.execute("INSERT INTO text_idx (id, title, book, author, date, chapter, verse, passage, link, documentType) SELECT id, title, book, author, date, chapter, verse, passage, link, documentType FROM texts;")
+        print ("inserted data into virtual table")
 
 @app.route('/')
 def hello_world():
-    wt = search.word_tokenizer
+    print ("Hello world")
+    search.word_tokenizer
+    print ("word_tokenizers")
     return json.dumps({"name": "test"})
 
 
 if __name__ == '__main__':
-    tokenize()
+    if os.environ.get('WERKZEUG_RUN_MAIN') == 'true':
+        tokenize()
     app.run(debug=True, host='0.0.0.0')
+    
+#tracker.print_diff()
