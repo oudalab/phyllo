75ce8ba4bbe0f5ff2055a5a2363d3f5a4b086ccf
Jordan Nguyen <jordan.nguyen-1@ou.edu>
2017-05-31 18:45:25 -0500

    Rimbaud fix
    - Rimbaud now parses poetry line-by-line correctly.
    - README.md updated to include links to the repository.

--- phyllo/extractors/rimbaudDB.py
+++ phyllo/extractors/rimbaudDB.py
@@ -2,74 +2,72 @@ import sqlite3
 import urllib
 import re
 from urllib.request import urlopen
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
 from phyllo.phyllo_logger import logger
-import nltk
-from itertools import cycle
 
-nltk.download('punkt')
-
-from nltk import sent_tokenize
-
-def parseRes2(soup, title, url, cur, author, date, collectiontitle):
-    chapter = '-'
-    j = 1
-    [e.extract() for e in soup.find_all('br')]
-    [e.extract() for e in soup.find_all('center')]
-    [e.extract() for e in soup.find_all('table')]
-    getp = soup.find_all('p')
-    i = 0
-    k=1
-    s1=[]
-    s=''
-    s2=[]
-    for p in getp:
-        # make sure it's not a paragraph without the main text
-        try:
-            if p['class'][0].lower() in ['border', 'shortborder', 'smallboarder', 'margin',
-                                         'internal_navigation', 'pagehead']:  # these are not part of the main t
-                continue
-        except:
-            pass
-        if i<4:
-            sen=p.text
-            sen=sen.strip()
-            s1=sen.split('\n')
-            l=0
-            while l+1<len(s1):
-                s=s1[l]+' '+s1[l+1]
-                s2.append(s)
-                l+=2
-            s2.append(s1[len(s1)-1])
-            k=1
-            for h in s2:
-                sentn=h
-                num=k
-                cur.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
-                            (None, collectiontitle, title, 'Latin', author, date, chapter,
-                             num, sentn, url, 'prose'))
-                k+=1
-        i+=1
 
 def main():
-    # get proper URLs
-    siteURL = 'http://www.thelatinlibrary.com'
-    biggsURL = 'http://www.thelatinlibrary.com/rimbaud.html'
-    biggsOPEN = urllib.request.urlopen(biggsURL)
-    biggsSOUP = BeautifulSoup(biggsOPEN, 'html5lib')
-    textsURL = []
-
-    title='VER ERAT'
-
-    author = 'Arthur Rimbaud'
-    author = author.strip()
-    collectiontitle='ARTHUR RIMBAUD'
-    date = '1854-1891'
+    # The collection URL below.
+    collURL = 'http://www.thelatinlibrary.com/rimbaud.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = "ARTHUR RIMBAUD"
+    date = collSOUP.span.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
+    date=date.strip()
+    textsURL = [collURL]
 
     with sqlite3.connect('texts.db') as db:
         c = db.cursor()
         c.execute("DELETE FROM texts WHERE author = 'Arthur Rimbaud'")
-        parseRes2(biggsSOUP, title, biggsURL, c, author, date, collectiontitle)
+
+        for url in textsURL:
+            chapter = 0
+            verse = 0
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            getp = textsoup.find_all('p')
+            title = "VER ERAT"
+            for p in getp:
+                # make sure it's not a paragraph without the main text
+                try:
+                    if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                                 'internal_navigation', 'smallborder']:  # these are not part of the main t
+                        continue
+                except:
+                    pass
+
+                brtags = p.findAll('br')
+                verses = []
+                try:
+                    try:
+                        firstline = brtags[0].previous_sibling.strip()
+                    except:
+                        firstline = brtags[0].previous_sibling.previous_sibling.strip()
+                    verses.append(firstline)
+                except:
+                    pass
+                if brtags is not None:
+                    chapter += 1
+                    tx = p.get_text().strip()
+                    if tx != '' or tx is not None:
+                        verses.append(tx)
+                for br in brtags:
+                    try:
+                        text = br.next_sibling.next_sibling.strip()
+                    except:
+                        text = br.next_sibling.strip()
+                    if text is None or text == '' or text.isspace():
+                        continue
+                    verses.append(text)
+                for v in verses:
+                    if v.strip().startswith("Neo-"):
+                        continue
+                    # verse number assignment.
+                    verse += 1
+                    c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                              (None, colltitle, title, 'Latin', author, date, chapter,
+                               verse, v.strip(), url, 'poetry'))
 
 
 if __name__ == '__main__':
