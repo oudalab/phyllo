9b720653aec03ccd6718bcba939fd692d8c692e4
Jordan Nguyen <jordan.nguyen-1@ou.edu>
2017-06-04 13:39:36 -0500

    Sannazaro, Tunger fixes
    - Sannazaro code parses poetry lines properly
    - Tunger's code reworked from the ground in order to account for
    sections and subsections properly.
    - README.md includes repository links for Sannazaro, Tunger and Xylander
    and Ruaeus. Ruaeus' status has been changed to "almost complete" due to
    its inherent organizational issues that will need looking into
    eventually.

--- phyllo/extractors/sannazaroDB.py
+++ phyllo/extractors/sannazaroDB.py
@@ -1,132 +1,93 @@
 import sqlite3
 import urllib
-import re
 from urllib.request import urlopen
-from bs4 import BeautifulSoup, NavigableString
+from bs4 import BeautifulSoup
 from phyllo.phyllo_logger import logger
-import nltk
-from itertools import cycle
-
-nltk.download('punkt')
-
-from nltk import sent_tokenize
-
-def parseRes2(soup, title, url, cur, author, date, collectiontitle):
-    chapter = '-'
-    [e.extract() for e in soup.find_all('br')]
-    j = 1
-    inv = ['i', 'u','b']
-    get = strip_tags(soup, inv)
-    getp = get.find_all('p')[:-1]
-    i=0
-    if url=='http://www.thelatinlibrary.com/sannazaro1.html':
-        for p in getp:
-            # make sure it's not a paragraph without the main text
-            try:
-                if p['class'][0].lower() in ['border', 'shortborder', 'smallboarder', 'margin',
-                                             'internal_navigation']:  # these are not part of the main t
-                    continue
-            except:
-                pass
-            sen = p.text
-            sen = sen.strip()
-            if i==0:
-                if sen.isupper():
-                    chapter = sen
-                    i+=1
-            elif sen.isupper():
-                chapter = sen
-                i+=1
-                if p.b:
-                    chapter=chapter+' '+p.b.text.strip()
-            else:
-                l = 1
-                for s in sent_tokenize(sen):
-                    sentn = s
-                    num = l
-                    cur.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
-                                (None, collectiontitle, title, 'Latin', author, date, chapter,
-                                 num, sentn, url, 'prose'))
-                    l += 1
-    else:
-        for p in getp:
-            # make sure it's not a paragraph without the main text
-            try:
-                if p['class'][0].lower() in ['border', 'shortborder', 'smallboarder', 'margin',
-                                             'internal_navigation']:  # these are not part of the main t
-                    continue
-            except:
-                pass
-            if p.b:
-                chapter=p.b.text
-            else:
-                sen=p.text
-                k=1
-                for s in sent_tokenize(sen):
-                    sentn=s
-                    num=k
-                    cur.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
-                                (None, collectiontitle, title, 'Latin', author, date, chapter,
-                                 num, sentn, url, 'prose'))
-                    k+=1
 
 
-
-def strip_tags(soup, invalid_tags):
-    for tag in soup.findAll(True):
-        if tag.name in invalid_tags:
-            s = ""
-
-            for c in tag.contents:
-                if not isinstance(c, NavigableString):
-                    c = strip_tags(str(c), invalid_tags)
-                s += str(c)
-
-            tag.replaceWith(s)
-
-    return soup
-
-def main():
-    # get proper URLs
+def getBooks(soup):
     siteURL = 'http://www.thelatinlibrary.com'
-    biggsURL = 'http://www.thelatinlibrary.com/sannazaro.html'
-    biggsOPEN = urllib.request.urlopen(biggsURL)
-    biggsSOUP = BeautifulSoup(biggsOPEN, 'html5lib')
     textsURL = []
-
-    for a in biggsSOUP.find_all('a', href=True):
+    # get links to books in the collection
+    for a in soup.find_all('a', href=True):
         link = a['href']
-        textsURL.append("{}/{}".format(siteURL, link))
+        textsURL.append("{}/{}".format(siteURL, a['href']))
 
-    # remove some unnecessary urls
+    # remove unnecessary URLs
     while ("http://www.thelatinlibrary.com/index.html" in textsURL):
         textsURL.remove("http://www.thelatinlibrary.com/index.html")
         textsURL.remove("http://www.thelatinlibrary.com/classics.html")
         textsURL.remove("http://www.thelatinlibrary.com/neo.html")
     logger.info("\n".join(textsURL))
+    return textsURL
 
-    author = biggsSOUP.title.string
-    author = author.strip()
-    collectiontitle = biggsSOUP.p.contents[0].strip()
-    date = biggsSOUP.span.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
-    date=date.strip()
 
-    title = []
-    for link in biggsSOUP.findAll('a'):
-        if (link.get('href') and link.get('href') != 'index.html' and link.get('href') != 'neo.html' and link.get(
-                'href') != 'classics.html'):
-            title.append(link.string)
+def main():
+    # The collection URL below.
+    collURL = 'http://www.thelatinlibrary.com/sannazaro.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = author.upper()
+    date = collSOUP.span.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
 
-    i = 0
+    textsURL = getBooks(collSOUP)
 
     with sqlite3.connect('texts.db') as db:
         c = db.cursor()
         c.execute("DELETE FROM texts WHERE author = 'Jacopo Sannazaro'")
-        for u in textsURL:
-            uOpen = urllib.request.urlopen(u)
-            gestSoup = BeautifulSoup(uOpen, 'html5lib')
-            parseRes2(gestSoup, title[i], u, c, author, date, collectiontitle)
-            i = i + 1
+
+        for url in textsURL:
+            chapter = -1
+            verse = 0
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            try:
+                title = textsoup.title.string.split(';')[1].strip()
+            except:
+                title = textsoup.title.string.strip()
+            title = title.replace("Sannazaro: ", "")
+            getp = textsoup.find_all('p')
+
+            for p in getp:
+                # make sure it's not a paragraph without the main text
+                try:
+                    if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                                 'internal_navigation']:  # these are not part of the main t
+                        continue
+                except:
+                    pass
+                # find chapter
+                chapter_f = p.find('b')
+                if chapter_f is not None:
+                    chapter = p.get_text().strip()
+                    verse = 0
+                    continue
+                else:
+                    brtags = p.findAll('br')
+                    verses = []
+                    try:
+                        try:
+                            firstline = brtags[0].previous_sibling.strip()
+                        except:
+                            firstline = brtags[0].previous_sibling.previous_sibling.strip()
+                        verses.append(firstline)
+                    except:
+                        pass
+                    for br in brtags:
+                        try:
+                            text = br.next_sibling.next_sibling.strip()
+                        except:
+                            text = br.next_sibling.strip()
+                        if text is None or text == '' or text.isspace():
+                            continue
+                        verses.append(text)
+                for v in verses:
+                    # verse number assignment.
+                    verse += 1
+                    c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                              (None, colltitle, title, 'Latin', author, date, chapter,
+                               verse, v, url, 'poetry'))
 
 
 if __name__ == '__main__':
--- phyllo/extractors/tungerDB.py
+++ phyllo/extractors/tungerDB.py
@@ -11,73 +11,74 @@ nltk.download('punkt')
 
 from nltk import sent_tokenize
 
-def parseRes2(soup, title, url, cur, author, date, collectiontitle):
-    chapter = '-'
-    [e.extract() for e in soup.find_all('br')]
-    j = 1
-    inv = ['i', 'u', 'b']
-    get = strip_tags(soup, inv)
-    getp = get.find_all('p')[:-1]
-    i=1
-    s1=[]
-    s2=[]
-    for p in getp:
+
+# Case 1: Sections split by numbers (Roman or not) followed by a period, or bracketed. Subsections split by <p> tags
+def parsecase1(ptags, c, colltitle, title, author, date, URL):
+    # ptags contains all <p> tags. c is the cursor object.
+    chapter = '-1'
+    verse = 1
+
+    for p in ptags:
         # make sure it's not a paragraph without the main text
         try:
-            if p['class'][0].lower() in ['border', 'shortborder', 'smallboarder', 'margin',
-                                         'internal_navigation', 'pagehead']:  # these are not part of the main t
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
                 continue
         except:
             pass
-        sen=p.text
-        sen=sen.strip()
-        if sen!='':
-            if sen[0].isdigit():
-                sen = sen[3:].strip()
-        i=1
-        for s in sent_tokenize(sen):
-            sentn=s
-            num=i
-            chapter=str(j)
-            cur.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
-                        (None, collectiontitle, title, 'Latin', author, date, chapter,
-                         num, sentn, url, 'prose'))
-            i+=1
-        j+=1
+        passage = ''
+        text = p.get_text().strip()
+        # Skip empty paragraphs. and skip the last part with the collection link.
+        if len(text) <= 0 or text.startswith('Neo\n'):
+            continue
+        text = re.split('^([IVX]+)\.\s|^([0-9]+)\.\s|^\[([IVXL]+)\]\s|^\[([0-9]+)\]\s', text)
+        for element in text:
+            if element is None or element == '' or element.isspace():
+                text.remove(element)
+        # The split should not alter sections with no prefixed roman numeral.
+        if len(text) > 1:
+            i = 0
+            while text[i] is None:
+                i += 1
+            chapter = text[i]
+            i += 1
+            while text[i] is None:
+                i += 1
+            passage = text[i].strip()
+            verse = 1
+        else:
+            passage = text[0]
+            verse += 1
+        # check for that last line with the author name that doesn't need to be here
+        if passage.startswith('Neo'):
+            continue
+        c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                  (None, colltitle, title, 'Latin', author, date, chapter,
+                   verse, passage.strip(), URL, 'prose'))
 
-def strip_tags(soup, invalid_tags):
-    for tag in soup.findAll(True):
-        if tag.name in invalid_tags:
-            s = ""
-
-            for c in tag.contents:
-                if not isinstance(c, NavigableString):
-                    c = strip_tags(str(c), invalid_tags)
-                s += str(c)
-
-            tag.replaceWith(s)
-
-    return soup
 
 def main():
-    # get proper URLs
-    siteURL = 'http://www.thelatinlibrary.com'
-    biggsURL = 'http://www.thelatinlibrary.com/tunger.html'
-    biggsOPEN = urllib.request.urlopen(biggsURL)
-    biggsSOUP = BeautifulSoup(biggsOPEN, 'html5lib')
-    textsURL = []
+    # The collection URL below. In this example, we have a link to Cicero.
+    collURL = 'http://www.thelatinlibrary.com/tunger.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = author.upper()
+    date = "1486"
 
-    title='AUGUSTIN TÜNGER FACETIAE LATINAE ET GERMANICAE'
-
-    author = 'Augustin Tunger'
-    author = author.strip()
-    collectiontitle='FACETIAE LATINAE ET GERMANICAE'
-    date = '1486'
+    textsURL = [collURL]
 
     with sqlite3.connect('texts.db') as db:
         c = db.cursor()
-        c.execute("DELETE FROM texts WHERE author = 'Scaliger'")
-        parseRes2(biggsSOUP, title, biggsURL, c, author, date, collectiontitle)
+        c.execute("DELETE FROM texts WHERE author='Augustin Tünger'")
+        for url in textsURL:
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            title = 'FACETIAE LATINAE ET GERMANICAE'
+            getp = textsoup.find_all('p')
+            parsecase1(getp, c, colltitle, title, author, date, url)
+
+    logger.info("Program runs successfully.")
 
 
 if __name__ == '__main__':
