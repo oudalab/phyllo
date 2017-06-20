f465b83905b13cd7835df221a2acfacd5d78f650
Katy Felkner <katy.felkner@ou.edu>
2017-06-19 22:30:55 -0500

    scraper for Roman Inscriptions
    scraper for Roman Inscritiptions in the Miscellany table. Also a test upload to make sure I am using git correctly.

---
+++ phyllo/extractors/roman_inscriptionsDB.py
@@ -0,0 +1,65 @@
+import sqlite3
+import urllib
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+def main():
+    # The collection URL below.
+    collURL = 'http://thelatinlibrary.com/inscriptions.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = "various authors"
+    colltitle = collSOUP.title.string.strip()
+    date = "no date found"
+
+    textsURL = [collURL]
+
+    with sqlite3.connect('texts.db') as db:
+        c = db.cursor()
+        c.execute("DELETE FROM texts WHERE title = 'Inscriptiones'")
+
+
+        for url in textsURL:
+            chapter = "-1"
+            verse = 0
+            title = colltitle
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+
+            getp = textsoup.find_all('p')
+
+            for p in getp:
+
+                # make sure it's not a paragraph without the main text
+                try:
+                    if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallborder', 'margin',
+                                                 'internal_navigation']:  # these are not part of the main t
+                        continue
+                except:
+                    pass
+                if p.find('b') is not None:
+                    chapter = p.find('b').string.strip()
+                    continue
+
+                verses = []
+
+                text = p.get_text()
+                if text.startswith("The Miscellany"):
+                    continue
+
+                try:
+                    verse = text.split(":")[0]
+                    verses.append(text.split(":")[1])
+                except:
+                    pass
+
+                for v in verses:
+                    c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                              (None, colltitle, title, 'Latin', author, date, chapter,
+                               verse, v.strip(), url, 'poetry'))
+
+
+
+if __name__ == '__main__':
+    main()
