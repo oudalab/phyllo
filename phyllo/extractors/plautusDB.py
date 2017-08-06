3f419ca569094e7519febc879fa6d26b78ad3a33
Jordan Nguyen <jordan.nguyen-1@ou.edu>
2017-08-06 14:36:30 -0500

    Finishing the Latin table
    - Plautus: completed. Messy, but it will have to do. Some verses aren't
    actually numeric, and as a result I've left the verse counter to count
    from the top of each chapter, while leaving in whatever verse it was
    denoted in the passage.
    - Valerius Maximus: works as intended. We could afford to split
    sentences here but I don't believe it's necessary.

---
+++ phyllo/extractors/plautusDB.py
@@ -0,0 +1,94 @@
+import sqlite3
+import urllib
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from phyllo.phyllo_logger import logger
+
+
+# This case parses poetry
+def parsePoem(ptags, c, colltitle, title, author, date, url):
+    chapter = -1
+    verse = 0
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        # find chapter
+        chapter_f = p.find('b')
+        test = p.get_text().strip()
+        if chapter_f is not None and test.startswith('PERS') or test.startswith('ARGVM') \
+                or test.startswith('ACTV') or test.startswith('PROLOGV') or \
+                test.startswith('<') or test.startswith('DINIA'):
+            chapter = p.get_text().strip()
+            verse = 0
+            continue
+        else:
+            verses = p.get_text().split('\n')
+        for v in verses:
+            if v is None:
+                continue
+            if v == '' or v.isspace() or v.strip() == 'Plautus' or v.strip().startswith('The'):
+                continue
+            # verse number assignment.
+            verse += 1
+            c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                      (None, colltitle, title, 'Latin', author, date, chapter,
+                       verse, v.strip(), url, 'poetry'))
+
+
+def getBooks(soup):
+    siteURL = 'http://www.thelatinlibrary.com'
+    textsURL = []
+    # get links to books in the collection
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        textsURL.append("{}/{}".format(siteURL, a['href']))
+
+    # remove unnecessary URLs
+    while ("http://www.thelatinlibrary.com/index.html" in textsURL):
+        textsURL.remove("http://www.thelatinlibrary.com/index.html")
+        textsURL.remove("http://www.thelatinlibrary.com/classics.html")
+    logger.info("\n".join(textsURL))
+    return textsURL
+
+
+# main code
+def main():
+    # The collection URL below. In this example, we have a link to Cicero.
+    collURL = 'http://www.thelatinlibrary.com/plautus.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = collSOUP.h1.string.strip()
+    date = collSOUP.h2.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
+
+    textsURL = getBooks(collSOUP)
+
+    with sqlite3.connect('texts.db') as db:
+        c = db.cursor()
+        c.execute(
+        'CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY, title TEXT, book TEXT,'
+        ' language TEXT, author TEXT, date TEXT, chapter TEXT, verse TEXT, passage TEXT,'
+        ' link TEXT, documentType TEXT)')
+        c.execute("DELETE FROM texts WHERE author='Plautus'")
+        for url in textsURL:
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            try:
+                title = textsoup.title.string.split(':')[1].strip()
+            except:
+                title = textsoup.title.string.strip()
+            getp = textsoup.find_all('p')
+            parsePoem(getp, c, colltitle, title, author, date, url)
+            logger.info('Parsed '+ url)
+
+    logger.info("Program runs successfully.")
+
+
+if __name__ == '__main__':
+    main()
---
+++ phyllo/extractors/valeriusmaximusDB.py
@@ -0,0 +1,100 @@
+import sqlite3
+import urllib
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+from phyllo.phyllo_logger import logger
+
+
+def parsecase2(ptags, c, colltitle, title, author, date, URL):
+    # ptags contains all <p> tags. c is the cursor object.
+    chapter = 0
+    verse = '-1'
+    # entry deletion is done in main()
+    for p in ptags:
+        # make sure it's not a paragraph without the main text
+        try:
+            if p['class'][0].lower() in ['border', 'pagehead', 'shortborder', 'smallboarder', 'margin',
+                                         'internal_navigation']:  # these are not part of the main t
+                continue
+        except:
+            pass
+        passage = ''
+        text = p.get_text().strip()
+        if text.startswith("Valerius\n"):
+            continue
+        # Skip empty paragraphs.
+        if len(text) <= 0:
+            continue
+        text = re.split('^([0-9]+\.[0-9]+\.[0-9])|^([0-9]+\.[0-9]+\.[e][x][t]\.[0-9])', text)
+        for element in text:
+            if element is None or element == '' or element.isspace():
+                text.remove(element)
+        chapter = text[0]
+        if chapter is None:
+            chapter = text[1]
+        verse = 0
+        for item in (text[1:]):
+            if item is None:
+                continue
+            if item.isspace() or item == '' or item == chapter:
+                continue
+            else:
+                verse += 1
+                passage = item
+                c.execute("INSERT INTO texts VALUES (?,?,?,?,?,?,?, ?, ?, ?, ?)",
+                          (None, colltitle, title, 'Latin', author, date, chapter,
+                           verse, passage.strip(), URL, 'prose'))
+
+
+def getBooks(soup):
+    siteURL = 'http://www.thelatinlibrary.com'
+    textsURL = []
+    # get links to books in the collection
+    for a in soup.find_all('a', href=True):
+        link = a['href']
+        textsURL.append("{}/{}".format(siteURL, a['href']))
+
+    # remove unnecessary URLs
+    while ("http://www.thelatinlibrary.com/index.html" in textsURL):
+        textsURL.remove("http://www.thelatinlibrary.com/index.html")
+        textsURL.remove("http://www.thelatinlibrary.com/classics.html")
+    logger.info("\n".join(textsURL))
+    return textsURL
+
+
+# main code
+def main():
+    # The collection URL below. In this example, we have a link to Cicero.
+    collURL = 'http://www.thelatinlibrary.com/valmax.html'
+    collOpen = urllib.request.urlopen(collURL)
+    collSOUP = BeautifulSoup(collOpen, 'html5lib')
+    author = collSOUP.title.string.strip()
+    colltitle = collSOUP.h1.string.strip()
+    date = collSOUP.h2.contents[0].strip().replace('(', '').replace(')', '').replace(u"\u2013", '-')
+
+    textsURL = getBooks(collSOUP)
+
+    with sqlite3.connect('texts.db') as db:
+        c = db.cursor()
+        c.execute(
+        'CREATE TABLE IF NOT EXISTS texts (id INTEGER PRIMARY KEY, title TEXT, book TEXT,'
+        ' language TEXT, author TEXT, date TEXT, chapter TEXT, verse TEXT, passage TEXT,'
+        ' link TEXT, documentType TEXT)')
+        c.execute("DELETE FROM texts WHERE author='Valerius Maximus'")
+        for url in textsURL:
+            openurl = urllib.request.urlopen(url)
+            textsoup = BeautifulSoup(openurl, 'html5lib')
+            try:
+                title = textsoup.title.string.split(':')[1].strip()
+            except:
+                title = textsoup.title.string.strip()
+            getp = textsoup.find_all('p')
+
+            parsecase2(getp, c, colltitle, title, author, date, url)
+
+    logger.info("Program runs successfully.")
+
+
+if __name__ == '__main__':
+    main()
